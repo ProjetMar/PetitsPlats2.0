ac407722402677e559aceceaf57f47d2f5d36665
marwa mezghani <marwamezghani29@gmail.com>
2024-05-29 11:32:21 +0200

    j'ai rendu cette branche comme le main (je dois la changer en array methode )

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -10,59 +10,97 @@ async function getRecettes() {
 }
 class IndexPage{
     constructor(recettes){
-        this.recettes = recettes; 
-        this.recettesAfficher = [];
-        this.recipe_ids_show = [];
+        this.recettes = recettes;
+        this.list_receipt_filtred_by_txt = [];
+        this.recettesAfficher = this.recettes;
         this.listTagSelect = [];
-        this.recipe_ids_after_search = [];
-        this.recipe_ids_after_tags = [];
-        this.inputPrincipale = document.getElementById('input1');
-        document.getElementById('searhButton').addEventListener('click', ()=>{
-            this.btnRecherchePrincipale()
-        })
-        this.inputPrincipale.addEventListener('keydown',(e)=>{
+        
+        let inputPrincipale = document.getElementById('input1');
+        inputPrincipale.addEventListener('keydown',(e)=>{
             if(e.key == "Enter"){
                 this.btnRecherchePrincipale()
             }
+        });
+        document.getElementById('searhButton').addEventListener('click', ()=>{
+            this.btnRecherchePrincipale()
+        });
+        document.getElementById('clearBtnInput1').addEventListener('click', ()=>{
+            this.remove_all_tags();
+            this.backToInitial();
         })
+       
         // pour faire revenir la liste à son etat intial 
         document.querySelectorAll('.liste-block input').forEach((element)=>element.addEventListener('input',(e)=>{
              if(e.currentTarget.value == ''){
                 this.displayListe()
              }
         }))
-        document.getElementById('clearBtnInput1').addEventListener('click', ()=>{
-            this.backToInitial()
-        })
+        
     }
+    
+    filter_by_text(txt){
+        // filtrer toutes les recettes par expression
+        let recettesAfficherPrinc = [];
+        for(let i=0; i< this.recettes.length; i++){
+            if (this.containString(this.recettes[i].name.toLowerCase(), txt) || 
+            (this.containString(this.recettes[i].description.toLowerCase(), txt)) || (this.verifListeIngredients(this.recettes[i].ingredients, txt)) ){
+                recettesAfficherPrinc[recettesAfficherPrinc.length] = this.recettes[i];
+            }
+        }
+        this.list_receipt_filtred_by_txt = recettesAfficherPrinc;
+        this.recettesAfficher = this.list_receipt_filtred_by_txt;
+    }
+
+    filter_by_tags(){
+        this.recettesAfficher = this.list_receipt_filtred_by_txt;
+        this.listTagSelect.forEach((tag)=>{
+            this.recettesAfficher = this.recettesAfficher.filter((recette)=>{
+                return this.verifExistance(recette.ingredients, 'ingredient', tag) || 
+                this.verifExistanceU(recette.ustensils, tag) ||
+                 recette.appliance == tag;
+            })
+        }) 
+    }
+
+    remove_all_tags(){
+        if(this.listTagSelect.length > 0){
+            document.querySelectorAll('.tag').forEach((element)=>{
+                element.remove()
+            })
+            this.listTagSelect.forEach((element)=>{   
+                document.getElementById(element).classList.add('tag-select');
+                document.getElementById(element).querySelector('span').classList.remove('d-block');
+                document.getElementById(element).querySelector('span').classList.add('d-none');     
+            })
+
+        }
+        this.listTagSelect = [];
+    }
+    
     btnRecherchePrincipale(){
+        this.remove_all_tags()
+        
         const input = document.getElementById('input1');
         if(input.value.length>=3){
-            this.recherche(input.value.toLowerCase())
-            this.get_recipe_ids_search(input.value.toLowerCase())
+            this.filter_by_text(input.value.toLowerCase());
+            if(this.recettesAfficher.length == 0){
+                this.displayNoReceiptFound(`Aucune recette ne contient " ${input.value} " vous pouvez chercher (tarte aux pommes)`)
+            }else{
+                this.displayData();
+                this.displayListe();
+            }
         }else{
             this.backToInitial();
         }
     }
+
     backToInitial(){
-        this.recipe_ids_after_search=[];
-        this.get_recipe_ids_tags();
-        if(this.recipe_ids_after_tags.length > 0){
-            this.recipe_ids_show = this.recipe_ids_after_tags;
-        }else{
-            this.recipe_ids_show =[];
-        }
-        this.getERecettesAfficher();
+        this.recettesAfficher = this.recettes;
         this.displayData();
         this.displayListe();
+        
     }
-    getERecettesAfficher(){
-        if(this.recipe_ids_show.length>0){
-            this.recettesAfficher = this.recettes.filter((recette)=>this.recipe_ids_show.includes(recette.id))
-        }else{
-            this.recettesAfficher = this.recettes
-        }
-    }
+
     // la methode ftatMap permet de extraire les ingredients des tab et aussi de creer un tableau contient les 
        //elements des sous tableau du tableau  
        // new set permet d'elever la repetition des elements 
@@ -75,16 +113,7 @@ class IndexPage{
     get ustensils(){
         return([...new Set(this.recettesAfficher.map(recette=> recette.ustensils).flat())])
     }
-    get titres(){
-        return (this.recettesAfficher.map(recette=>recette.name))
-    }
-    get descriptions(){
-        return(this.recettesAfficher.map(recette=>recette.description))
-    }
-    get ingredientsElements(){
-        return(this.recettesAfficher.map(recette=> recette.ingredients))
-    }
-    displayNonRecette(message){
+    displayNoReceiptFound(message){
         const recettesSection = document.querySelector(".sectionCard");
         recettesSection.innerHTML='';
         const p = document.createElement('p')
@@ -92,7 +121,6 @@ class IndexPage{
         recettesSection.appendChild(p)
     }
     displayListe(){
-       this.getERecettesAfficher();
          let map ={
             "ingredients": this.ingredients,
             "Appareils": this.Appareils,
@@ -118,110 +146,48 @@ class IndexPage{
         }
     }
     displayData(){
+        //Afficher les cartes des recettes trouvées
         const recettesSection = document.querySelector(".sectionCard");
         recettesSection.innerHTML='';
-        this.getERecettesAfficher();
         this.recettesAfficher.forEach((recette) => {
             const recetteModel =new Card(recette);
             const CardDom = recetteModel.getCardRecette();
             recettesSection.appendChild(CardDom);
         });
-    
+        
+        //Afficher le nombre des recettes trouvées
         const nbrRecettes = this.recettesAfficher.length;
         const domNbrRecettes = document.querySelector('.fontStyle');
         domNbrRecettes.textContent = `${nbrRecettes} recettes`;
     }
-    ActualIdRecettesAfficher(){
-        if(this.recipe_ids_show.length == 0){
-            this.recettesAfficher.forEach((recette)=>{
-                this.recipe_ids_show.push(recette.id)
-            })   
-        }else{
-            this.recipe_ids_show = [];
-            this.recettesAfficher.forEach((recette)=>{
-                this.recipe_ids_show.push(recette.id)
-            }) 
-        }
-    }
     //methode ou j'ai utilisé les methodes native
-    // myIncludes(mainString, subString) {
-    //     for (let i = 0; i <= mainString.length - subString.length; i++) {
-    //         let match = true;
-    //         for (let j = 0; j < subString.length; j++) {
-    //             if (mainString[i + j] !== subString[j]) {
-    //                 match = false;
-    //                 break;
-    //             }
-    //         }
-    //         if (match) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    // verifListeIngredients (ingredientsElement, expression){
-    //     let existe = false ; 
-    //     let i = 0 ; 
-    //     while(i<ingredientsElement[i].length && existe == false){
-    //         if(this.myIncludes(ingredientsElement[i].toLowerCase(),expression)){
-    //             existe = true;
-    //         }
-    //         i = i+1;
-    //     }
-    //     return(existe)
-    // }
-    verifIncludes(listIngredients, ingredient, expression){
-        let include = false;
-        listIngredients.forEach((option)=>{
-            if(option[ingredient].toLowerCase().includes(expression)){
-                include = true;
+    containString(mainString, subString) {
+        for (let i = 0; i <= mainString.length - subString.length; i++) {
+            let match = true;
+            for (let j = 0; j < subString.length; j++) {
+                if (mainString[i + j] !== subString[j]) {
+                    match = false;
+                    break;
+                }
             }
-        })
-        return(include)
-    }
-    //filtre apres saisie champ principale 
-    filter_Recettes_ChampPrincipale(expression){
-        let recettesAfficherPrinc = [];
-        // for(let i=0; i< this.titres.length; i++){
-        //     if (this.myIncludes(this.titres[i].toLowerCase(), expression) || 
-        //     (this.myIncludes(this.descriptions[i].toLowerCase(), expression)) || (this.verifListeIngredients(this.ingredientsElements[i], expression)) ){
-        //         // recettesAfficherPrinc.push(this.recettesAfficher[i])
-        //         recettesAfficherPrinc[recettesAfficherPrinc.length] = this.recettesAfficher[i];
-        //     }
-        // }
-        recettesAfficherPrinc = this.recettesAfficher.filter((recette)=>{
-            return(recette.name.toLowerCase().includes(expression) || recette.description.toLowerCase().includes(expression) || 
-            this.verifIncludes(recette.ingredients, 'ingredient', expression))
-        })
-        return(recettesAfficherPrinc)
-    }
-    //methode ou j'ai utilisé seulement les methodes native 
-    get_recipe_ids_search(expression){
-        this.recipe_ids_show=[]
-        this.getERecettesAfficher();
-        this.recettesAfficher = this.filter_Recettes_ChampPrincipale(expression);
-        // this.recettesAfficher.forEach((recette)=>{
-        //     this.recipe_ids_after_search.push(recette.id)
-        // }) 
-        for (let i = 0; i < this.recettesAfficher.length; i++) {
-            this.recipe_ids_after_search[this.recipe_ids_after_search.length] = this.recettesAfficher[i].id;
+            if (match) {
+                return true;
+            }
         }
-        console.log(this.recipe_ids_after_search)
+        return false;
     }
-    //methode ou j'ai utilisé seulement les methodes native 
-    recherche(expression){
-        this.getERecettesAfficher();
-        this.recettesAfficher = this.filter_Recettes_ChampPrincipale(expression);
-        this.ActualIdRecettesAfficher()
-        console.log(this.recipe_ids_show)
-        if(this.recipe_ids_show.length == 0){
-            this.displayNonRecette(`Aucune recette ne contient " ${expression} " vous pouvez chercher (tarte aux pommes)`)
-            console.log(this.recipe_ids_show == [])
-        }else{
-            return(this.displayData(), this.displayListe())
+    verifListeIngredients (ingredientsElement, expression){
+        let existe = false ; 
+        let i = 0 ; 
+        while(i<ingredientsElement[i].length && existe == false){
+            if(this.containString(ingredientsElement[i].toLowerCase(),expression)){
+                existe = true;
+            }
+            i = i+1;
         }
-        
+        return(existe)
     }
+
     getTagSelect(tagSelect, parentList){
         console.log(parentList)
         const tag = new TagDom(tagSelect);
@@ -239,16 +205,13 @@ class IndexPage{
     rechercheTag(e){
         let tag = e.currentTarget.textContent;
         let element =  e.currentTarget.parentElement.parentElement;
-        console.log(element)
-        console.log(tag)
         if(this.listTagSelect.includes(tag) == false){
             this.getTagSelect(tag, element.parentElement);
             console.log(document.querySelector('.tag button'))
-            this.getERecettesAfficher()
             this.listTagSelect.push(tag);
             this.filter_recettes_tag(tag)
-            this.ActualIdRecettesAfficher();
-            return(this.displayData(), this.displayListe())
+            this.displayData(); 
+            this.displayListe();
         }   
     }
     verifExistance(option, sousElement, tag){
@@ -269,38 +232,11 @@ class IndexPage{
         })
         return(existe)
     }
-    get_recipe_ids_tags(){
-        this.recipe_ids_show=[];
-        this.getERecettesAfficher();
-        console.log(this.listTagSelect)
-        this.listTagSelect.forEach((tag)=>{
-            this.filter_recettes_tag(tag)
-        })
-        this.recettesAfficher.forEach((recette)=>{
-            this.recipe_ids_after_tags.push(recette.id)
-        }) 
-
-    }
     filtreTagSupprimer(tagSupprimer){
         document.getElementById(tagSupprimer).classList.remove('tag-select');
         console.log(this.listTagSelect)
-        if(this.recipe_ids_after_search.length >0){
-            this.recipe_ids_show = this.recipe_ids_after_search;
-        }else{
-            this.recipe_ids_show = [];
-        }
-        // this.recipe_ids_show = []
-        this.getERecettesAfficher();
         this.listTagSelect= this.listTagSelect.filter(item => item !== tagSupprimer);
-        console.log(this.listTagSelect)
-        this.listTagSelect.forEach((tag)=>{
-            this.recettesAfficher = this.recettesAfficher.filter((recette)=>{
-                return this.verifExistance(recette.ingredients, 'ingredient', tag) || 
-                this.verifExistanceU(recette.ustensils, tag) ||
-                 recette.appliance == tag;
-            })
-        })
-        this.ActualIdRecettesAfficher();
+        this.filter_by_tags();
         return(this.displayData(), this.displayListe())
     }
     //fonction qui recherche apres avoir supprimer un tag
@@ -313,9 +249,12 @@ class IndexPage{
     }
 }
 async function init() {
-    // Récupère les datas des photographes
+    // Récupère la liste des recettes
     const recettes = await getRecettes();
+
     const indexpage = new IndexPage(recettes)
+    
+    //Afficher la liste des recettes
     indexpage.displayData()
     indexpage.displayListe()
 }
